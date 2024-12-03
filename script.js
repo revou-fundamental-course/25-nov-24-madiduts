820dc358773c12c2fc94215c03d02d010c70bede
madiduts <mahdisutejo7@gmail.com>
2024-12-03 11:14:08 +0700

    Input dari pengguna

---
+++ script.js
@@ -0,0 +1,10 @@
+// Contoh kode JavaScript
+console.log("Hello, world!");
+
+function greet() {
+  alert("Selamat Datang di Halaman Ini!");
+}
+
+function apaAja() {
+  console.log("Selamat datang di web ini!");
+}
